3853234d43ae6b8ea1f0deaddcb866ce0ae4ba75
Serkan Akman <serkan.akman@boranka.com>
2020-05-31 15:18:08 +0300

    Axios ile data fetch olayları - Covid 19 api entegrasyonu

--- src/components/FetchData2.js
+++ src/components/FetchData2.js
@@ -35,41 +35,55 @@ function FetchData2() {
 
     const [state, dispatch] = useReducer(reducer, initialState);
     const [input, setinput] = useState("Turkey");
-    
+    const [ulke, setulke] = useState("Turkey");
 
     useEffect(() => {
         axios
             .get(url)
             .then(data => dispatch({
                 type: 'FETCH_SUCCESS',
-                payload: (data.data.Countries.filter(ulke => ulke.Country === input)[0]) == undefined ? {} : data.data.Countries.filter(ulke => ulke.Country === (input == "" ? "Germany": input))[0]
+                payload: data.data.Countries
             }))
             .catch({ type: 'FETCH_FAIL' });
-    }, [state.data.Country]);
+
+    }, []);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => {
+        handleSelect(input);
+    }, [state.data, input])
 
     const handleChange = (e) => {
         e.preventDefault();
-        setinput(e.target.value)
+        setinput(e.target.value);
     }
-    console.log(state, input)
+    const handleSelect = async (ulke) => {
+        try {
+            const data = await state.data && state.data.filter(cntry =>( cntry.Country).toLowerCase() === ulke.toLowerCase());
+            // console.log(typeof state.data[1].Country,  state.data[1].Country);
+            setulke(data[0]);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <div>
-        <b>**FetchData2.js</b><br/>
+            <b>**FetchData2.js</b><br />
 
             {
                 state.loading ? 'Yükleniyor...' :
                     <div>
-                        <input type="text" value={input} onChange={e => handleChange(e)}/>
-                        <div className="Country"><p><b>Ülke adı: </b>{state.data.Country}</p></div>
-                        <div className="Country"><p><b>Kısa adı: </b>{state.data.CountryCode}</p></div>
-                        <div className="Country"><p><b>Sorgu tarihi: </b>{state.data.Date}</p></div>
-                        <div className="Country"><p><b>Yeni vaka sayısı: </b>{state.data.NewConfirmed}</p></div>
-                        <div className="Country"><p><b>Yeni ölüm: </b>{state.data.NewDeaths}</p></div>
-                        <div className="Country"><p><b>Bugün iyileşen: </b>{state.data.NewRecovered}</p></div>
-                        <div className="Country"><p><b>Toplam vaka: </b>{state.data.TotalConfirmed}</p></div>
-                        <div className="Country"><p><b>Toplam ölüm: </b>{state.data.TotalDeaths}</p></div>
-                        <div className="Country"><p><b>Toplam iyileşen: </b>{state.data.TotalRecovered}</p></div>
+                        <input type="text" value={input} onChange={e => handleChange(e)} />
+                        <div className="Country"><p><b>Ülke adı: </b>{typeof ulke === "object" && ulke.Country}</p></div>
+                        <div className="Country"><p><b>Kısa adı: </b>{typeof ulke === "object" && ulke.CountryCode}</p></div>
+                        <div className="Country"><p><b>Sorgu tarihi: </b>{typeof ulke === "object" && ulke.Date}</p></div>
+                        <div className="Country"><p><b>Yeni vaka sayısı: </b>{typeof ulke === "object" && ulke.NewConfirmed}</p></div>
+                        <div className="Country"><p><b>Yeni ölüm: </b>{typeof ulke === "object" && ulke.NewDeaths}</p></div>
+                        <div className="Country"><p><b>Bugün iyileşen: </b>{typeof ulke === "object" && ulke.NewRecovered}</p></div>
+                        <div className="Country"><p><b>Toplam vaka: </b>{typeof ulke === "object" && ulke.TotalConfirmed}</p></div>
+                        <div className="Country"><p><b>Toplam ölüm: </b>{ typeof ulke === "object" && ulke.TotalDeaths}</p></div>
+                        <div className="Country"><p><b>Toplam iyileşen: </b>{ typeof ulke === "object" && ulke.TotalRecovered}</p></div>
                     </div>
             }
 
